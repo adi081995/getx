5a8e57ce7d9c805bf23ebfe05b31fd5c997a3a0d
Aditya Salunkhe <aditya.salunkhe95@gmail.com>
2024-07-09 15:31:29 +0530

    Frontend Changes

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,7 +1,7 @@
 import Banner from "./Banner";
 // import Brand from "./Brand";
 import ChartArea from "./ChartArea";
-import Contribution from "./Contribution";
+// import Contribution from "./Contribution";
 import DownloadArea from "./DownloadArea";
 import FAQ from "./Faq";
 import FeatureOne from "./FeatureOne";
@@ -17,7 +17,7 @@ const Home = () => {
       <>
          <Banner />
          <FeatureThree />
-         <Contribution />
+         {/* <Contribution /> */}
          {/* <Brand /> */}
          <FeatureOne />
          <FeatureTwo />
